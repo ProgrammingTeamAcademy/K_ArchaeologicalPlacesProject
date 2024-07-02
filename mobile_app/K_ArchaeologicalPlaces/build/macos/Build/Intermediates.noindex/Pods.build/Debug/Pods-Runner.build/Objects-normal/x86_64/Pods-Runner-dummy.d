3b70d7af73f1a6dafbc8f794c804bf1ae871b657
Hossam Al-Maraghy <161968436+hossamalmaraghy@users.noreply.github.com>
2024-07-03 02:09:05 +0300

    Delete mobile_app directory

--- mobile_app/K_ArchaeologicalPlaces/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/HOSSAM/Desktop/Dart/flutter_project/GitHub/K_ArchaeologicalPlacesProject/mobile_app/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/HOSSAM/Desktop/Dart/flutter_project/GitHub/K_ArchaeologicalPlacesProject/mobile_app/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m
